676dab20c6b3d3e939012ae792808e80eea6a99e
seo-bo-seung <81345746+seo-bo-seung@users.noreply.github.com>
2024-03-05 21:03:30 +0900

    Title: 배열의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240305/배열의 합/sum-of-array.cpp
+++ 240305/배열의 합/sum-of-array.cpp
@@ -5,7 +5,7 @@ int main() {
     int arr[4][4];
     for(int i=0; i<4; i++){
         for(int j=0; j<4; j++){
-            scanf("%d", arr[i][j]);
+            scanf("%d", &arr[i][j]);
         }
     }
 
