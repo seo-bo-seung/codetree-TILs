ad8d75586e10eebc6bbb7e1c78db1faa7da39b58
seo-bo-seung <81345746+seo-bo-seung@users.noreply.github.com>
2024-03-06 01:42:48 +0900

    Title: 살 수 있는 물건, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin>>a;
+    if(a>=3000) cout<<"book";
+    else if(a>=1000) cout<<"mask";
+    else cout<<"no";
+    return 0;
+}
