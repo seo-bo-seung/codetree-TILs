9fab40cf293506c35aa3a3f03620c2450dae3306
seo-bo-seung <81345746+seo-bo-seung@users.noreply.github.com>
2024-03-06 22:28:48 +0900

    Title: 짝수만 출력 2, Time: 1004ms, Memory: 73MB, Status: Time Limit Exceed - CodeTree

---
+++ 240306/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int b,a;
+    std::cin>>b>>a;
+    for(int i=b; i>=a; i++ ){
+        if(i%2==0) std::cout<<i<<" ";
+    }
+    return 0;
+}
