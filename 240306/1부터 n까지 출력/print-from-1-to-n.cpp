c0d99a8d72d3537c19e0e74ec7deac429d3b56f4
seo-bo-seung <81345746+seo-bo-seung@users.noreply.github.com>
2024-03-06 20:37:29 +0900

    Title: 1부터 n까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/1부터 n까지 출력/print-from-1-to-n.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    for(int i; i<n; i++){
+        cout<<i+1<<" ";
+    }
+    return 0;
+}
