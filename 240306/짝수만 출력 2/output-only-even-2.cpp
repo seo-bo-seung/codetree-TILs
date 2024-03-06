43ab72a211504470dca221600657a3bf4db29630
seo-bo-seung <81345746+seo-bo-seung@users.noreply.github.com>
2024-03-06 22:29:45 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/짝수만 출력 2/output-only-even-2.cpp
+++ 240306/짝수만 출력 2/output-only-even-2.cpp
@@ -4,7 +4,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int b,a;
     std::cin>>b>>a;
-    for(int i=b; i>=a; i++ ){
+    for(int i=b; i>=a; i-- ){
         if(i%2==0) std::cout<<i<<" ";
     }
     return 0;
