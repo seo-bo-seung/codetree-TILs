1b991cb606fa6f304c0887851e6437dca2aa83f7
seo-bo-seung <81345746+seo-bo-seung@users.noreply.github.com>
2024-03-09 14:19:17 +0900

    Title: 가장 짧은 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240309/가장 짧은 문자열/shortest-string.cpp
+++ 240309/가장 짧은 문자열/shortest-string.cpp
@@ -3,11 +3,13 @@
 using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
-    string a,b,c;
-    int small=100;
+    string a;
+    int small=20;
+    int max=0;
     for(int i=0; i<3; i++){
         cin>>a;
         if(a.size()<small) small=a.size();
+        if(a.size()>max) max=a.size();
     }
-    cout<<small;
+    cout<<max-small;
 }
