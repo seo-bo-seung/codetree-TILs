4abbd02302605009d8fb15ba56f49008ae4e57f8
seo-bo-seung <81345746+seo-bo-seung@users.noreply.github.com>
2024-03-09 15:30:58 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240309/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+void pprint(int n){
+    for(int i=0; i<n; i++){
+        printf("%s","12345^&*()_");
+        printf("\n");
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    pprint(4);
+    return 0;
+}
