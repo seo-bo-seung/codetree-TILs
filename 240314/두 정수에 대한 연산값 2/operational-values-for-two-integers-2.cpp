000b9ac422bdc76887212e92903f3df488cb6a54
seo-bo-seung <81345746+seo-bo-seung@users.noreply.github.com>
2024-03-14 15:55:39 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240314/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void IsAns(int &a, int &b){
+    if(a>b){
+        b=b+10;
+        a*=2;
+    }
+    else{
+        a+=10;
+        b*=2;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin>>a>>b;
+    IsAns(a,b);
+    cout<<a<<" "<<b;
+    return 0;
+}
