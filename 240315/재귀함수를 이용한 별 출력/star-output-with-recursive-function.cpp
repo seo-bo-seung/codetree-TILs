8eb13ad0365d9402067518a3c5e155cb619bd88a
seo-bo-seung <81345746+seo-bo-seung@users.noreply.github.com>
2024-03-15 23:00:14 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240315/재귀함수를 이용한 별 출력/star-output-with-recursive-function.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void IsAns(int n){
+    if(n==0) return;
+    IsAns(n-1);
+    for(int i=0; i<n; i++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d",&n);
+    IsAns(n);
+    return 0;
+}
