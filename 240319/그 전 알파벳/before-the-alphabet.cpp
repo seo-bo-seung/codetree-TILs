97d62055922bd52a5774bb53765cac505d0396e8
seo-bo-seung <81345746+seo-bo-seung@users.noreply.github.com>
2024-03-19 10:42:45 +0900

    Title: 그 전 알파벳, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240319/그 전 알파벳/before-the-alphabet.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char a;
+    scanf("%c",&a);
+    if(a=='a'){
+        printf("%c",'z');
+    }
+    else{
+        printf("%c",a-1);
+    }
+    return 0;
+}
