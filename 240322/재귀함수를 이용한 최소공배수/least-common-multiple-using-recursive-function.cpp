2d2c94e61c02c5089cd07b916df265cce9902e18
seo-bo-seung <81345746+seo-bo-seung@users.noreply.github.com>
2024-03-22 17:19:56 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240322/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
+++ 240322/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
@@ -3,17 +3,22 @@ using namespace std;
 int n;
 int arr[11];
 
+int gcd(int a, int b) {
+    if(b == 0) return a;  // b == 0이면 더 이상 최대공약수를 갖지 않기 때문에 a를 반환한다.
+    else gcd(b, a%b);  // 유클리드 호제법을 활용하여서 a, b의 최대 공약수를 b, r(a%b)로 치환한다.
+}
+
+// gcd는 a, b의 최대공약수이다.
+int lcm(int a, int b) {
+    return a*b/gcd(a,b);
+}
+
 int Ans(int n){
-    int sum=1;
+    int tmp=1;
     for(int i=0; i<n; i++){
-        if(sum%arr[i]==0){
-            sum= sum;
-        }
-        else{
-            sum= sum*arr[i];
-        }
+        tmp=lcm(tmp,arr[i]);
     }
-    return sum;
+    return tmp;
 }
 
 int main() {
