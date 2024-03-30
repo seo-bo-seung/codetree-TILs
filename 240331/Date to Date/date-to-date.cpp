02a45ebd5b2da572e83846edba7360eb7aee924b
seo-bo-seung <81345746+seo-bo-seung@users.noreply.github.com>
2024-03-31 03:16:19 +0900

    Title: Date to Date, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240331/Date to Date/date-to-date.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m1,d1,m2,d2;
+    cin>>m1>>d1>>m2>>d2;
+    int elapsed_days=0;
+    int num_of_days[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+    while(true){
+        if(m2==m1 && d2==d1) break;
+        elapsed_days++;
+        d1++;
+
+        if(d1>num_of_days[m1]){
+            m1++;
+            d1=0;
+        }
+    }
+    cout<<elapsed_days;
+    return 0;
+}
