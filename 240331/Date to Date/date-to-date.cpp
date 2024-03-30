b0812357da421189d4beb31bd0470be5c5ec3cca
seo-bo-seung <81345746+seo-bo-seung@users.noreply.github.com>
2024-03-31 03:18:48 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240331/Date to Date/date-to-date.cpp
+++ 240331/Date to Date/date-to-date.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int m1,d1,m2,d2;
     cin>>m1>>d1>>m2>>d2;
-    int elapsed_days=0;
+    int elapsed_days=1;
     int num_of_days[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
 
     while(true){
