09147cbc1c938c1570cd5a16830aabeb7a10f100
seo-bo-seung <81345746+seo-bo-seung@users.noreply.github.com>
2024-04-01 01:33:57 +0900

    Title: 원점으로부터의 거리, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240401/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class XY{
+    public:
+        int x,y,num;
+
+        XY(int x,int y, int num){
+            if(x<0) this->x=-x;
+            else this->x=x;
+            if(y<0) this->y=-y;
+            else this->y=y;
+            this->num=num;
+        }
+        XY(){}
+};
+
+bool cmp(const XY &a, const XY &b){
+    if(a.x+a.y==b.x+b.y){
+        return a.num<b.num;
+    }
+    return a.x+a.y<b.x+b.y;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    int x,y;
+    XY arr[100];
+    for(int i=0; i<n; i++){
+        cin>>x>>y;
+        arr[i]=XY(x,y,i+1);
+    }
+    sort(arr,arr+n,cmp);
+    for(int i=0; i<n; i++){
+        cout<<arr[i].num<<endl;
+    }
+    return 0;
+}
