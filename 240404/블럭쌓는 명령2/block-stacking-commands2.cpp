92e2b69c99d011596e198684473128fdb5487f1d
seo-bo-seung <81345746+seo-bo-seung@users.noreply.github.com>
2024-04-04 23:38:38 +0900

    Title: 블럭쌓는 명령2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240404/블럭쌓는 명령2/block-stacking-commands2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,k;
+    cin>>n>>k;
+    int arr[101]={0};
+    int a,b;
+    for(int i=1; i<=k; i++){
+        cin>>a>>b;
+        for(int j=a; j<=b; j++){
+            arr[j]++;
+        }
+    }
+    int max=0;
+    for(int i=1; i<=k; i++){
+        if(arr[i]>max) max=arr[i];
+    }
+    cout<<max;
+
+    return 0;
+}
